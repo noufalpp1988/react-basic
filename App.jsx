29216ee765952745bad84ab2df3e1472a31ca488
Noufal P P <a-7755@LE1047.local>
2018-06-06 00:30:06 +0530

    -stateless example

--- App.jsx
+++ App.jsx
@@ -5,9 +5,30 @@ class App extends React.Component {
         return (
             <div>
                 <h1>Hello</h1>
+                <Header />
+                <Content />
             </div>
         );
     }
 }
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Header component</h2>
+            </div>
+        );
+    }
+}
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Content component</h2>
+            </div>
+        );
+    }
+}
+
 
 export default App;
